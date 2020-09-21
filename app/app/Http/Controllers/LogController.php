7e25806c73c8ce9790c95b60aea0fc5a57da6cfb
kb995 <t.kuboshima995@gmail.com>
2020-09-22 05:35:28 +0900

    認証処理追加

--- app/app/Http/Controllers/LogController.php
+++ app/app/Http/Controllers/LogController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Day;
 
 class LogController extends Controller
 {
-    //
+    public function logs() {
+        return view('logs');
+    }
 }
